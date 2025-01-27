d5b14fb621bae487fbcf92b61365afadfc94d6b8
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-01-27 19:17:10 +0100

    debugging

--- controller/postsController.js
+++ controller/postsController.js
@@ -18,7 +18,7 @@ exports.createPost = async (req, res, next) => {
         author,
         image,
         content: sanitizedContent,
-        user: {
+        userId: {
           connect: {
             id: userId,
           },
