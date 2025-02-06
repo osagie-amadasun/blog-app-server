f97985d6afd0e103a3a5d99c45966ab93fe70d42
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-02-06 21:20:13 +0100

    former password requirements were kinda ludicrous for a simple blog app project

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -26,14 +26,6 @@ router.post("/signup", [
   .withMessage("password is required")
   .isLength({ min: 6 })
   .withMessage("password must be at least 6 characters long")
-  .matches(/[a-z]/)
-  .withMessage("password must contain at least one lowercase letter")
-  .matches(/[A-Z]/)
-  .withMessage("password must contain at least one uppercase letter")
-  .matches(/[0-9]/)
-  .withMessage("password must contain at least one number")
-  .matches(/[$@#&!%*?]/)
-  .withMessage("password must contain at least one special character")
 ], usersController.signUp);
 router.post("/login", usersController.logIn);
 router.get("/getUsers", usersController.getAllUsers);
